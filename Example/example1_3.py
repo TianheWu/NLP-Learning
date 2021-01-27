9bfa6f88f1a5b48f0d52b37be1017907244e6b24
TianheWu <tianhe_wu@foxmail.com>
2021-01-27 22:21:44 +0800

    Example1_3

---
+++ Example/example1_3.py
@@ -0,0 +1,10 @@
+import torch
+
+
+def describe(x):
+    print("Type: {}".format(x.type()))
+    print("Shape/size: {}".format(x.shape))
+    print("Values: \n{}".format(x))
+
+
+describe(torch.Tensor(2, 3))
